f83d5c42b6f7d14da24713e537908bb5ec209246
yyprdqp527-ui <yyprdqp527@privaterelay.appleid.com>
2026-07-07 16:38:52 +0200

    Fix: Android build (MainActivity package path, JVM target, revert receive_sharing_intent to 1.8.0)

--- android/app/src/main/kotlin/com/reelr/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/reelr/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.clips
+package com.reelr.app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,13 +4,11 @@ allprojects {
         mavenCentral()
     }
 }
-
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
-
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
@@ -18,7 +16,20 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
 }
-
+subprojects {
+    if (project.name != "app") {
+        afterEvaluate {
+            extensions.findByName("android")?.let {
+                val androidExt = it as com.android.build.gradle.BaseExtension
+                androidExt.compileOptions.sourceCompatibility = JavaVersion.VERSION_17
+                androidExt.compileOptions.targetCompatibility = JavaVersion.VERSION_17
+            }
+            tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+                compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            }
+        }
+    }
+}
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
